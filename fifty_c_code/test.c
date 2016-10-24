56d96f795f93403818ea45c077b825148406404f
zpw <zpengwei@yeah.net>
2016-10-24 21:59:28 +0800

    add test.c for test

---
+++ fifty_c_code/test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: test.c
+    > Author: peter
+    > Mail: zpengwei@yeah.net 
+    > Created Time: 三  5/25 13:45:21 2016
+ ************************************************************************/
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	printf("hello world\n");
+	return 0;
+}
